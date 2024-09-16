bc68cb9ff4f4e166505174fa56d216a84351ef58
Rafael Bohrer Ávila <rafaelavila@ifsul.edu.br>
2024-09-16 18:12:16 +0000

    Solução 4

--- Main.java
+++ Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Main
  */
 public class Main {
     public static void main(String[] args) {
-        Carro cr = new Carro();
-        Caminhao cm = new Caminhao();
-        cr.buzine();
-        cm.buzine();
+        Scanner teclado = new Scanner(System.in);
+        Veiculo[] v = new Veiculo[2];
+        for (int i = 0; i < v.length; i++) {
+            System.out.printf("Qual o tipo do %do veículo? (1 = carro, 2 = caminhão): ", i + 1);
+            int resp = teclado.nextInt();
+            if (resp == 1) {
+                v[i] = new Carro();
+            } else {
+                v[i] = new Caminhao();
+            }
+        }
+        for (int i = 0; i < v.length; i++) {
+            v[i].buzine();
+        }
     }
 }
