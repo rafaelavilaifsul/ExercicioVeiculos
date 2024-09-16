74d55eaa4c90f60776fdccd418836a5c0c5d27a4
Rafael Bohrer Ávila <rafaelavila@ifsul.edu.br>
2024-09-16 18:29:40 +0000

    Agora usando ArrayList em vez de vetor

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -6,8 +7,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        Veiculo[] v = new Veiculo[2];
-        for (int i = 0; i < v.length; i++) {
+        ArrayList<Veiculo> v = new ArrayList<Veiculo>();
+        for (int i = 0; i < 2; i++) {
             System.out.printf("Qual o tipo do %do veículo? (1 = carro, 2 = caminhão): ", i + 1);
             int resp = teclado.nextInt();
             teclado.nextLine();
@@ -16,14 +17,14 @@ public class Main {
             System.out.print("Qual o modelo: ");
             String modelo = teclado.nextLine();
             if (resp == 1) {
-                v[i] = new Carro(marca, modelo);
+                v.add(new Carro(marca, modelo));
             } else {
-                v[i] = new Caminhao(marca, modelo);
+                v.add(new Caminhao(marca, modelo));
             }
         }
-        for (int i = 0; i < v.length; i++) {
-            System.out.println(v[i]);
-            v[i].buzine();
+        for (Veiculo x: v) {
+            System.out.println(x);
+            x.buzine();
         }
     }
 }
